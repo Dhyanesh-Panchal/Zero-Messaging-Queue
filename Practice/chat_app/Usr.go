c86302d5c9ba8e095b1954559b112fa31ebaa838
Dhyanesh-Panchal <dhynincoding2004@gmail.com>
2025-02-13 10:42:49 +0530

    Chat's Core working

--- Practice/chat_app/Usr.go
+++ Practice/chat_app/Usr.go
@@ -4,53 +4,74 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 
 	zmt "github.com/pebbe/zmq4"
 )
 
-func recieve_msg(context *zmt.Context) {
-	// Publisher socket
+const SERVER_IP string = "10.20.40.165"
+
+var reader = bufio.NewReader(os.Stdin)
+
+func recieve_msg(context *zmt.Context, name_channel chan string) {
+	// Subscriber socket connected to central server's 5002 port
 	reciever, _ := context.NewSocket(zmt.SUB)
 	defer reciever.Close()
-	// Connect at port 5002
-	reciever.Connect("tcp://localhost:5002")
+	reciever.Connect(fmt.Sprintf("tcp://%s:5002", SERVER_IP))
+
+	// Get the sender's name
 	fmt.Printf("Enter your name: ")
 	name, _ := reader.ReadString('\n')
+	name = strings.TrimSuffix(name, "\n")
+
+	// share the name to main routine
+	name_channel <- name
 
 	// Subscribe to self and broadcst
 	reciever.SetSubscribe(name)
 	reciever.SetSubscribe("broadcast")
 	for {
 		message, _ := reciever.Recv(0)
-		// time.Sleep(1 * time.Second)
-		fmt.Printf("Recieved: %s", string(message))
+		message = strings.TrimSuffix(message, "\n")
+
+		// Parse the message
+		message_split := strings.Split(message, " ")
+		_, sender_name, extracted_message := message_split[0], message_split[1], strings.Join(message_split[2:], " ")
+
+		if sender_name != name {
+			fmt.Printf("\n\nFrom %s: %s\n", sender_name, extracted_message)
+		}
 	}
 }
 
 // Just hava ma try karu chu
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
 
 	context, _ := zmt.NewContext()
 	defer context.Term()
 
-	// Reciever socket
+	// Message pushing socket connected to server at port 5001
 	server_snd, _ := context.NewSocket(zmt.PUSH)
 	defer server_snd.Close()
-	// Connect at port 5001
-	server_snd.Connect("tcp://localhost:5001")
+	server_snd.Connect(fmt.Sprintf("tcp://%s:5001", SERVER_IP))
 
+	name_channel := make(chan string)
 	// start a go routine for listening to published message
-	go recieve_msg(context)
-	for {
+	go recieve_msg(context, name_channel)
 
+	//wait for the slef declaration
+	self_name := <-name_channel
+	for {
 		fmt.Printf("Enter reciever name: ")
 		reciever_name, _ := reader.ReadString('\n')
+		reciever_name = strings.TrimSuffix(reciever_name, "\n")
+
 		fmt.Printf("Enter message: ")
 		msg, _ := reader.ReadString('\n')
+		msg = strings.TrimSuffix(msg, "\n")
 
-		server_snd.Send(fmt.Sprintf("%s %s", reciever_name, msg), 0)
+		server_snd.Send(fmt.Sprintf("%s %s %s", reciever_name, self_name, msg), 0)
 
 	}
 
